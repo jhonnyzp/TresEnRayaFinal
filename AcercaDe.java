ca3942fc108fdaaa17504ab71e2d9361e611862d
jhonnyzp <jhonnyzp@outlook.com>
2016-08-22 12:05:59 -0500

    Initial commit

---
+++ AcercaDe.java
@@ -0,0 +1,18 @@
+package com.example.tresenrayafinal;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+ 
+public class AcercaDe extends Activity {
+    /** Called when the activity is first created. */
+   
+	
+	@Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fileacercade);
+    }
+
+}
